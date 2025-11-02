3dec7f56f56f705e5d0d84e08fc6801bdd70d62a
chrisrenshaw79 <chrisrenshaw79@gmail.com>
2025-11-02 16:22:26 +0000

    Made changes to project structure and added dependencies

--- index.tsx
+++ index.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './src/index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
